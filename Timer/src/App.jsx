60772a1976a43a294daa0c500fdb503dfa4fa1f2
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-08-28 13:30:14 +0530

    Timer

--- Timer/src/App.jsx
+++ Timer/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import stopwatch from "./assets/stopwatch.svg";
 import "./index.css";
 
 const Timer = () => {
@@ -7,13 +8,15 @@ const Timer = () => {
 
   useEffect(() => {
     let interval = null;
+
     if (isActive) {
       interval = setInterval(() => {
         setSeconds((prev) => prev + 1);
-      }, 1000);
+      }, 10);
     } else {
       clearInterval(interval);
     }
+
     return () => clearInterval(interval);
   }, [isActive]);
 
@@ -27,18 +30,22 @@ const Timer = () => {
   };
 
   const formatTime = (s) => {
-    const minutes = Math.floor(s / 60);
-    const secs = s % 60;
-    return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(
+    const minutes = Math.floor(s / 6000);
+    const seconds = Math.floor((s % 6000) / 100);
+    const milliseconds = s % 100;
+
+    return `${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(
       2,
       "0"
-    )}`;
+    )} : ${String(milliseconds).padStart(2, "0")}`;
   };
 
   return (
     <div className="app-container">
       <div className="timer-card">
-        <h1 className="timer-circle">{formatTime(seconds)}</h1>
+        <img src={stopwatch} width={100} height={100} alt="" />
+        <h1>Timer</h1>
+        <h1 className="time-text">{formatTime(seconds)}</h1>
         <div className="btn-group">
           <button
             onClick={handleStartPause}
